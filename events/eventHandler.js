ac80617fe9735cb68aa91c9ff9de9b1e9bdb0090
Swelix <167325522+bless3dd@users.noreply.github.com>
2025-12-22 14:50:55 +0100

    update eventHandler.js

--- events/eventHandler.js
+++ events/eventHandler.js
@@ -1,18 +1,29 @@
-const memberEvents = require('./memberEvents');
-const messageEvents = require('./messageEvents');
-const channelEvents = require('./channelEvents');
-const commandHandler = require('./commandHandler');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = (client) => {
-    // Eventi membri
-    memberEvents(client);
-    
-    // Eventi messaggi
-    messageEvents(client);
-    
-    // Eventi canali
-    channelEvents(client);
-    
-    // Gestione comandi slash
-    commandHandler(client);
+    const eventsPath = path.join(__dirname);
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => 
+        file.endsWith('.js') && file !== 'eventHandler.js'
+    );
+
+    console.log('📋 Caricamento event handlers...');
+
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const eventHandler = require(filePath);
+        const eventName = file.split('.')[0];
+
+        console.log(`✅ Event handler caricato: ${eventName}`);
+
+        if (eventName === 'interactionCreate') {
+            client.on(eventName, async (interaction) => {
+                await eventHandler(interaction);
+            });
+        } else if (eventName === 'ready') {
+            client.once(eventName, () => eventHandler(client));
+        } else {
+            client.on(eventName, (...args) => eventHandler(client, ...args));
+        }
+    }
 };
