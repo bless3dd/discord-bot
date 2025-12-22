e556fdb4b17108dbbf68097e39cdd173d44e2072
Swelix <167325522+bless3dd@users.noreply.github.com>
2025-12-22 14:18:53 +0100

    update registerCommands.js

--- utils/registerCommands.js
+++ utils/registerCommands.js
@@ -1,18 +1,28 @@
 const { REST, Routes } = require('discord.js');
 const { CLIENT_ID, GUILD_ID, TOKEN } = require('../config');
-const moderationCommands = require('../commands/moderation.js');
-const infoCommands = require('../commands/info.js');
-const funCommands = require('../commands/fun.js');
+const fs = require('fs');
+const path = require('path');
 
 async function registerCommands(client) {
     try {
         console.log('📋 Registrazione comandi slash...');
 
-        const commands = [
-            ...moderationCommands.map(cmd => cmd.data.toJSON()),
-            ...infoCommands.map(cmd => cmd.data.toJSON()),
-            ...funCommands.map(cmd => cmd.data.toJSON())
-        ];
+        const commands = [];
+        const commandsPath = path.join(__dirname, '..', 'commands');
+        
+        // Leggi tutti i file .js nella cartella commands
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+        
+        console.log('File trovati:', commandFiles);
+        
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const commandModule = require(filePath);
+            
+            if (Array.isArray(commandModule)) {
+                commands.push(...commandModule.map(cmd => cmd.data.toJSON()));
+            }
+        }
 
         const rest = new REST({ version: '10' }).setToken(TOKEN);
 
